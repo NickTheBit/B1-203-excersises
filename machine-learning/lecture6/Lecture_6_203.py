f6e5d326bfd2d5a486f77ad2db3e9b23efc6aabb
NickTheBit <nikosgklu@gmail.com>
2023-10-04 09:48:49 +0200

    Template for lecture6

---
+++ machine-learning/lecture6/Lecture_6_203.py
@@ -0,0 +1,9 @@
+# ###################################
+# Group ID : 203
+# Members : Malthe Boelskift, Louis Ildal, Guillermo Gutierrez Bea, Nikolaos Gkloumpos.
+# Date : 04/11/2023
+# Lecture: Linear discriminants
+# Dependencies: numpy, matplotlib, sklearn, scipy
+# Python version: 3.11.4
+# Functionality: Excersise on classification.
+# ###################################
