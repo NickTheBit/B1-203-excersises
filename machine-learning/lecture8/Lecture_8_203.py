fbb150befc40c2bd18ac9e8696176d169b0b5838
NickTheBit <nikosgklu@gmail.com>
2023-10-18 11:51:13 +0200

    Initial excersise template

---
+++ machine-learning/lecture8/Lecture_8_203.py
@@ -0,0 +1,9 @@
+# ###################################
+# Group ID : 203
+# Members : Malthe Boelskift, Louis Ildal, Guillermo Gutierrez Bea, Nikolaos Gkloumpos.
+# Date : 18/10/2023
+# Lecture: 8 Neural networks
+# Dependencies: numpy, matplotlib, sklearn
+# Python version: 3.11.4
+# Functionality: 
+# ###################################
