50ba5e54cd414053c6af1a471c4ea266ed652fbb
NickTheBit <nikosgklu@gmail.com>
2023-11-09 09:46:20 +0100

    Added template for tracking

---
+++ machine-learning/lecture11/Lecture_11_203.py
@@ -0,0 +1,10 @@
+# ###################################
+# Group ID : 203
+# Members : Malthe Boelskift, Louis Ildal, Guillermo Gutierrez Bea, Nikolaos Gkloumpos.
+# Date : 18/10/2023
+# Lecture: 11
+# Dependencies: numpy, matplotlib, sklearn
+# Python version: 3.11.4
+# Functionality: 
+# ###################################
+
