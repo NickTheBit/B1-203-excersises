d32450b07fb97b3c23030600021b9b4231d4d00e
NickTheBit <nikosgklu@gmail.com>
2023-11-15 11:52:23 +0100

    Pushed new ml lecture template.

---
+++ machine-learning/lecture12/Lecture_12_203.py
@@ -0,0 +1,14 @@
+# ###################################
+# Group ID : 203
+# Members : Malthe Boelskift, Louis Ildal, Guillermo Gutierrez Bea, Nikolaos Gkloumpos.
+# Date : 18/10/2023
+# Lecture: 11
+# Dependencies: numpy, matplotlib, sklearn
+# Python version: 3.11.4
+# Functionality: 
+# ###################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import numba
+
