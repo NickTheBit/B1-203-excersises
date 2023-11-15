2eefe3f6029c89706f5d9df62990033e1af662a7
NickTheBit <nikosgklu@gmail.com>
2023-11-15 15:12:33 +0100

    Cleanup and updated source pictures.

--- machine-learning/lecture3/aliases.sh
+++
@@ -1,6 +0,0 @@
-workingDir="/home/nick/Projects/university/machine-learning/session3/"
-
-alias python='$workingDir/environment/bin/python3'
-alias pip='$workingDir/environment/bin/pip3'
-alias pip3='pip'
-alias python3='python'
