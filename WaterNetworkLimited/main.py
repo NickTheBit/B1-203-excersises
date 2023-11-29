ec91bfba05a10c933556b97f333fe3e7d2ffe902
Guillermp <145263791+Guillermp@users.noreply.github.com>
2023-11-29 10:54:35 +0100

    Update main.py + new features
    -Saves the last QTable (You can view it using heatmap.py)
    -Tells you when the QTable has converged, so when the difference compared to the previous error is smaller than a certain percentage specified by the threshold.

--- WaterNetworkLimited/main.py
+++ WaterNetworkLimited/main.py
@@ -6,26 +6,35 @@ import numpy as np
 import definitions as d
 import random as rd
 import matplotlib.pyplot as plt
+import pandas as pd 
+import copy
 
 # Q(time,tankLevel,pump enabled)
-QTable = np.ones((24, 19, 2))
+#QTable= np.random.randint(low=0, high=5, size=(24, 19,2))
+QTable = np.zeros((24, 19, 2))
 LevelHistory=[]
 ActionHistory=[]
-N_days=1000 # Number of days that we are running the simulation
+N_days=2*500 # Number of days that we are running the simulation
 
 
 def startup():
 	# Initializing environment
-	env = d.Enviro(1, 20,0) # arguments: initial tank level, pump status, noise status (either 0 or 1)
+	global prev_QTable
+	prev_QTable=0
+	never_entered=1
+	env = d.Enviro(1, 20,1) # arguments: initial tank level, pump status, noise status (either 0 or 1)
 
 	# Learning parameters
-	learning_rate = 0.07
-	discount_factor = 0.8
-	x_intersect=100 # day at which epsilon decays to 0
+	learning_rate = 0.2
+	discount_factor = 0.4
+	x_intersect=2*400 # day at which epsilon decays to 0
 	a=9/x_intersect
+	J=0
+	
 
 	for j in range(0,N_days+1):
 		# Looping over every state, lol
+		#learning_rate = 5/(j+1)
 		for i in range(0, 23+1):
 			# Mapping out the tank
 			tankState = env.getTankLevelDiscreteVariable()
@@ -44,25 +53,68 @@ def startup():
 			env.updateWaterLevel(i, currentAction*env.pumpFlowRate)
 			futureTankState= env.getTankLevelDiscreteVariable()  
 
+	
 			#We penalize more if we get out of
-			if currentAction==1 and futureTankState==8:
-				Jnext=env.cost(currentAction)*5
-			elif currentAction==0 and futureTankState==0:
-				Jnext=env.cost(currentAction)*5
-			else:
-				Jnext=env.cost(currentAction)*5
-				
+			Jnext=env.cost(currentAction)
+		
 			
 
 			# Updating the Q - Value
-			if i <=22:
-				QTable[i][tankState][currentAction] =(1-learning_rate)*QTable[i][tankState][currentAction]+learning_rate*(Jnext+discount_factor*QTable[i+1][futureTankState][np.argmin(QTable[i+1][futureTankState])]-QTable[i][tankState][currentAction])
+			if i <23:
+				QTable[i,tankState,currentAction] =(1-learning_rate)*QTable[i, tankState, currentAction]+learning_rate*(Jnext+discount_factor*QTable[i+1,futureTankState, np.argmin(QTable[i+1,futureTankState])]-QTable[i,tankState,currentAction])
 			else:
-				QTable[i][tankState][currentAction] = (1-learning_rate)*QTable[i][tankState][currentAction]+learning_rate*(Jnext+discount_factor*QTable[0][futureTankState][np.argmin(QTable[0][futureTankState])]-QTable[i][tankState][currentAction])
+				QTable[i, tankState, currentAction] = (1-learning_rate)*QTable[i,tankState,currentAction]+learning_rate*(Jnext+discount_factor*QTable[0,futureTankState, np.argmin(QTable[0,futureTankState])]-QTable[i,tankState,currentAction])
 
 			ActionHistory.append(currentAction*env.pumpFlowRate)
 			LevelHistory.append(env.currentTankLevel)
-	print(len(LevelHistory))
+
+			
+			if j==N_days:
+				J=abs(J+Jnext)
+				print(QTable)
+
+			# Check for convergence
+			difference = QTable - prev_QTable
+			absolute_error = np.abs(difference)
+			relative_error = absolute_error / (np.abs(QTable) + 1e-10)
+		
+			matrix_np = np.array(relative_error)
+			threshold=0.0001
+
+			prev_QTable = copy.copy(QTable)
+		
+			if(np.all((matrix_np < threshold)) and never_entered and j>0):
+				never_entered=0
+				convergenceDay=j
+				convergenceHour=i
+				print(f"Convergence day={convergenceDay}, hour={convergenceHour}")
+
+		
+		
+	print(f"G={J}")
+	
+
+	#To save the last QTable
+	# Convert the array to a DataFrame
+	#df = pd.DataFrame(np.max(QTable, axis=2))
+	df_action_0=pd.DataFrame(QTable[:,:,0])
+	df_action_1=pd.DataFrame(QTable[:,:,1])
+
+    # Save the DataFrame to a CSV file
+	df_action_0.to_csv('df_action_0.csv', index=False, header=False)
+	df_action_1.to_csv('df_action_1.csv', index=False, header=False)
+	
+	
+
+			
+
+
+
+
+		
+		
+
+	
 
 
 	
@@ -107,9 +159,12 @@ def startup():
 	plt.ylabel("Demand")  # Y-axis label
 	plt.title("Demand vs Water Level over the last day [L]")  # Title for the second subplot
 
+
 	# Adjust layout to prevent overlap
 	plt.tight_layout()
 	plt.show()
+
+	
 
 	
 	
